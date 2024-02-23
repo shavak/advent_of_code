89d4656897c4a2cbc0224566f75766af0fb27f8c
Shavak Sinanan <shavak@gmail.com>
2024-02-23 21:43:55 +0000

    Added solution for Day 4 Part (b).

--- 2023/day_04/src/lib.rs
+++ 2023/day_04/src/lib.rs
@@ -5,6 +5,7 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 use std::collections::HashSet;
+use std::collections::HashMap;
 
 pub fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where
@@ -14,7 +15,8 @@ where
     Ok(io::BufReader::new(file).lines())
 }
 
-pub fn card_points(card: String) -> u32 {
+fn num_matches(card: String) -> u32 {
+    // Computes the number of points on a single card.
     let v: Vec<&str> = card.split(" | ").collect();
     let u: Vec<&str> = v[0].split(": ").collect();
     let w: HashSet<u32> = HashSet::from_iter(
@@ -28,8 +30,26 @@ pub fn card_points(card: String) -> u32 {
         .split_whitespace()
         .map(|s| s.trim().parse::<u32>().unwrap());
     let mut m = 0;
-    for k in y {
-        m += if w.contains(&k) { 1 } else { 0 };
+    for j in y {
+        m += if w.contains(&j) { 1 } else { 0 };
     }
+    m
+}
+
+pub fn card_points(card: String) -> u32 {
+    let m = num_matches(card);
     if m == 0 { 0 } else { 2u32.pow(m - 1) }
+}
+
+pub fn update_card_count(card: String, k: u32, card_copies: &mut HashMap<u32, u32>) {
+    // Updates the count of each card.
+    let h: &mut u32 = card_copies.entry(k).or_insert(0u32);
+    *h += 1;
+    let c: u32 = (*h).clone();
+    let m = num_matches(card);
+    let mut q: &mut u32;
+    for i in 1..=m {
+        q = card_copies.entry(k + i).or_insert(0u32);
+        *q += c;
+    }
 }
--- 2023/day_04/src/main.rs
+++ 2023/day_04/src/main.rs
@@ -2,6 +2,7 @@
 // Author: Shavak Sinanan <shavak@gmail.com>
 
 use::std::path::*;
+use::std::collections::HashMap;
 use day_04::*;
 
 fn part_a(input_path: &Path) {
@@ -15,18 +16,21 @@ fn part_a(input_path: &Path) {
 }
 
 fn part_b(input_path: &Path) {
+    let mut k: u32 = 0;
+    let mut card_copies: HashMap<u32, u32> = HashMap::new();
     if let Ok(lines) = read_lines(input_path) {
         for line in lines.flatten() {
+            k += 1;
+            update_card_count(line, k, &mut card_copies);
         }
     }
-    let mut acc = 0;
-    println!("Part (b):\n<text here> = {acc}\n");
+    println!("Part (b):\nTotal number of cards = {}\n", card_copies.values().sum::<u32>());
 }
 
 fn main() {
     let input_root_dir = Path::new("input/");
     let input_filename_a = "input.txt";
-    let input_filename_b = "test_input.txt";
+    let input_filename_b = "input.txt";
     println!();
     part_a(input_root_dir.join(input_filename_a).as_path());
     part_b(input_root_dir.join(input_filename_b).as_path());
