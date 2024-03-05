3bff161836870eb5fe7c281a957912c23553a733
Shavak Sinanan <shavak@gmail.com>
2024-03-05 21:07:35 +0000

    Added solution for Day 6.

--- 2023/day_06/src/lib.rs
+++ 2023/day_06/src/lib.rs
@@ -1,4 +1,10 @@
-// Supporting crate for the solution to Advent of Code 2023 Day 4.
+// Supporting crate for the solution to Advent of Code 2023 Day 6.
 // Author: Shavak Sinanan <shavak@gmail.com>
 
+use std::cmp::*;
 
+pub fn num_record_breaks(t: f64, d: f64) -> u64 {
+    let p = 0.5 * t;
+    let q = (p * p - d).sqrt();
+    max((p + q - 1.0).ceil() as u64, 0) - max((p - q + 1.0).floor() as u64, 0) + 1
+}
--- 2023/day_06/src/main.rs
+++ 2023/day_06/src/main.rs
@@ -1,20 +1,41 @@
 // Solution to Advent of Code 2023 Day 6.
 // Author: Shavak Sinanan <shavak@gmail.com>
 
-use::std::path::*;
-use::std::collections::HashMap;
+use std::path::Path;
+use aoc_utils::*;
 use day_06::*;
 
 fn part_a(input_path: &Path) {
+    let mut acc: u64 = 1u64;
+    if let Ok(lines) = read_lines(input_path) {
+        let mut lines_f = lines.flatten();
+        let t: Vec<f64> = split_whitespace_parse::<f64>(&lines_f.next().unwrap());
+        let d: Vec<f64> = split_whitespace_parse::<f64>(&lines_f.next().unwrap());
+        let n = t.len();
+        for i in 0..n {
+            acc *= num_record_breaks(t[i], d[i]);
+        }
+    }
+    println!("Part (a):\nProduct = {acc}\n");
 }
 
 fn part_b(input_path: &Path) {
+    if let Ok(lines) = read_lines(input_path) {
+        let mut lines_f = lines.flatten();
+        let lnt = &lines_f.next().unwrap();
+        let i: usize = lnt.find(":").unwrap();
+        let t = lnt[i + 1..].split_whitespace().collect::<Vec<&str>>().join("").parse::<f64>().unwrap();
+        let lnd = &lines_f.next().unwrap();
+        let i: usize = lnd.find(":").unwrap();
+        let d = lnd[i + 1..].split_whitespace().collect::<Vec<&str>>().join("").parse::<f64>().unwrap();
+        println!("Part (b):\nNumber of ways to break record = {}", num_record_breaks(t, d))
+    }
 }
 
 fn main() {
     let input_root_dir = Path::new("input/");
-    let input_filename_a = "test_input.txt";
-    let input_filename_b = "test_input.txt";
+    let input_filename_a = "input.txt";
+    let input_filename_b = "input.txt";
     println!();
     part_a(input_root_dir.join(input_filename_a).as_path());
     part_b(input_root_dir.join(input_filename_b).as_path());
